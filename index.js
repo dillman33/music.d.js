017a6fb5e0d0d4fbab4ca386d4f2b4e7a3465fe0
funnbot <unconfigured@null>
2017-02-15 07:02:28 -0800

    ok

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const _url = require('url');
-const youtube_info = require('youtube-info');
-const resolve = require('soundcloud-resolve');
-const request = require('request');
 const Guild = require('./lib/Guild')
 const ut = require('./lib/util.js')
 
@@ -64,74 +60,4 @@ exports.addConnection = (connection, message) => {
     if (this.players[guildID]) throw new Error("Connection for the id: " + guildID + " already exists")
 
     players[message.guild.id] = new Guild(options, connection, message)
-}
-
-/* Create a voice channel connection
-yourbot.joinVoiceChannel(channelID).then(connection => {
-This must be run to initialize the bot into a voice channel
-textChannel is the channel in which Next song messages will be sent
-*/
-exports.connect = (guildID, voiceChannel, textChannel, connection) => {
-    con[guildId] = {
-        voiceChannel,
-        textChannel,
-        connection,
-        playlist: []
-    };
-}
-
-exports.addSong = (guildID, url, user) => {
-    return new Promise((res, rej) => {
-        if (!con[guildID]) return rej('You must connect to a channel first.');
-        let type = exports.linkType(url);
-        if (type === "yt-playlist" || type === "sc-playlist" || type === "nal") return rej('InvalidLink: Don\'t request playlists');
-        if (type === "yt") {
-            youtubeInfo(url).then(data => {
-                let song = {
-                    title: data.title,
-                    owner: data.owner,
-                    stream: data.url,
-                    url: data.url,
-                    duration: data.duration * 1000,
-                    regionsAllowed: data.regionsAllowed
-                };
-                con[guildID].playlist.push(song);
-                res(song);
-            }).catch(err => {
-                rej(err);
-            })
-        }
-        if (type === "sc") {
-            if (!opt || !opt.scKey) return rej('Soundcloud links need a soundcloud api key');
-            soundcloudInfo(url).then(data => {
-                let song = {
-                    title: data.title,
-                    owner: data.user.username,
-                    stream: data.stream_url,
-                    url: url,
-                    duration: data.duration,
-                    regionsAllowed: "all"
-                }
-                con[guildID].playlist.push(song);
-
-            }).catch(err => {
-                rej(err);
-            })
-        }
-    });
-}
-
-function youtubeInfo(url) {
-    
-}
-
-function soundcloudInfo(url) {
-    return new Promise((res, rej) => {
-        let type = linkType(url);
-        if (type === "yt-playlist" || type === "sc-playlist" || type === "nal" || type === "yt") return rej('InvalidLink: only soundcloud tracks.')
-        resolve(opt.scKey, url, (err, data) => {
-            if (err) return rej('Failed to resolve soundcloud link.');
-            return res(data);
-        });
-    })
 }
